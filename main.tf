0454f01b3e93811c60d04a94ffd261a661e146f4
sadegh baradaran <57625478+sadeghBaradaran@users.noreply.github.com>
2023-07-14 23:01:13 +0330

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,11 @@
 
 provider "aws" {
   region                      = "eu-central-1"
-  access_key                  = "devopshobbies"
-  secret_key                  = "devopshobbies"
+  access_key                  = "distributed"
+  secret_key                  = "distributed"
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
-
 
   endpoints {
     apigateway     = "http://localhost:4566"
@@ -33,14 +32,13 @@ provider "aws" {
     ssm            = "http://localhost:4566"
     stepfunctions  = "http://localhost:4566"
     sts            = "http://localhost:4566"
-
   }
 }
 
 ######### DynamoDB #########
 
-resource "aws_dynamodb_table" "  devopshobbies-dev-devops-consumer-events" {
-  name           = "  devopshobbies-dev-devops-consumer-events"
+resource "aws_dynamodb_table" "justdice-dev-devops-consumer-events" {
+  name           = "justdice-dev-devops-consumer-events"
   read_capacity  = "20"
   write_capacity = "20"
   hash_key       = "Id" 
@@ -51,24 +49,92 @@ resource "aws_dynamodb_table" "  devopshobbies-dev-devops-consumer-events" {
   }
 }
 
+resource "aws_dynamodb_table" "students" {
+  name           = "students"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+}
+
+resource "aws_dynamodb_table" "lessons" {
+  name           = "lessons"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+
+  attribute {
+    name = "MasterId"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "MasterIdIndex"
+    hash_key           = "MasterId"
+    projection_type    = "ALL"
+    read_capacity      = 20
+    write_capacity     = 20
+  }
+}
+
+resource "aws_dynamodb_table" "masters" {
+  name           = "masters"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "Id"
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+
+  attribute {
+    name = "Name"
+    type = "S"
+  }
+
+  global_secondary_index {
+    name               = "Name-index"
+    hash_key           = "Name"
+    write_capacity     = 5
+    read_capacity      = 5
+    projection_type    = "INCLUDE"
+
+    non_key_attributes = [
+      "age",
+      "address",
+      # Add more non-key attributes here if needed
+    ]
+  }
+}
+
 ######### SNS Topic #########
 
 resource "aws_sns_topic" "sns_topic" {
-  name      =   "  devopshobbies-devops-producer-events"
+  name = "justdice-dev-devops-producer-events"
 }
+
 
 ######### SQS Queue #########
 
 resource "aws_sqs_queue" "queue" {
-  name      =   "  devopshobbies-devops-consumer-events"
+  name = "justdice-dev-devops-consumer-events"
 }
 
 ######### SNS Subscription #########
 
 resource "aws_sns_topic_subscription" "private_http_subscription" {
-  topic_arn = aws_sns_topic.sns_topic.arn   
-  protocol  = "sqs"
-  endpoint  = aws_sqs_queue.queue.arn 
+  topic_arn              = aws_sns_topic.sns_topic.arn
+  protocol               = "sqs"
+  endpoint               = aws_sqs_queue.queue.arn
   endpoint_auto_confirms = true
 }
-
